7e0b82c4b368483c1ab8f47453c4c6dc2e6ae9a6
Alexandre Dumont <adumont@gmail.com>
2017-08-17 19:41:43 +0200

    Push consumptionsByCycle to MQTT

--- simyo_mqtt.py
+++ simyo_mqtt.py
@@ -4,13 +4,26 @@ import random
 import re
 import sys
 import paho.mqtt.client as paho
-from urllib.parse import urlparse
+#from urllib.parse import urlparse
+import urlparse
+import argparse
+import pyDes
+import base64
+import urllib
+import urllib2
+import hmac
+import hashlib
+import json
+import datetime
+import pprint
+import collections
+from time import time
 
 # Load Heroku Config Variables (https://devcenter.heroku.com/articles/config-vars)
 SIMYO_USER = os.environ['SIMYO_USER']
 SIMYO_PASS = os.environ['SIMYO_PASS']
 SIMYO_NUMB = os.environ['SIMYO_NUMB']
-DEBUG = os.getenv('DEBUG', False) == "True"
+VERBOSE = os.getenv('DEBUG', False) == "True"
 
 # Define event callbacks
 def on_connect(mosq, obj, rc):
@@ -37,15 +50,148 @@ mqttc.on_subscribe = on_subscribe
 
 # Parse CLOUDMQTT_URL (or fallback to localhost)
 url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://localhost:1883')
-url = urlparse(url_str)
+url = urlparse.urlparse(url_str)
 
 # Connect
 mqttc.username_pw_set(url.username, url.password)
 mqttc.connect(url.hostname, url.port)
 
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def getApiSig(url):
+        dig = hmac.new(b'f25a2s1m10', msg='f25a2s1m10' + url.lower(), digestmod=hashlib.sha256).digest()
+        return url + "&apiSig=" + dig.encode('hex')
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def simyopass():
+        k = pyDes.triple_des("25d1d4cb0a08403e2acbcbe0", pyDes.ECB, "\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
+        d = urllib.quote(base64.b64encode(k.encrypt(SIMYO_PASS)) + '\n')
+        #print "Encrypted: %r" % d
+        #print "Decrypted: %r" % k.decrypt(base64.b64decode(urllib.unquote(d)))
+        return d
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def writeFile(filename, content):
+        in_file = open(filename,"wb")
+        in_file.write(content)
+        in_file.close()
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def convert(data):
+        # http://stackoverflow.com/q/1254454/
+        if isinstance(data, basestring):
+                return str(data)
+        elif isinstance(data, collections.Mapping):
+                return dict(map(convert, data.iteritems()))
+        elif isinstance(data, collections.Iterable):
+                return type(data)(map(convert, data))
+        else:
+                return data
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def epoch2date(timestamp, format='%d/%m/%Y'):
+        timestamp = str(timestamp)[0:10]
+        return datetime.datetime.fromtimestamp(int(timestamp)).strftime(format)
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def api_request(url, data="", check=True):
+        kPublicKey="a654fb77dc654a17f65f979ba8794c34"
+
+        if url[-1:] == "?":
+                url=url + "publicKey=" + kPublicKey
+        else:
+                url=url + "&publicKey=" + kPublicKey
+
+        url=getApiSig(url)
+
+        if VERBOSE:
+                print "URL: " + url
+
+        if data=="":
+                req = urllib2.Request(url)
+        else:
+                req = urllib2.Request(url,data)
+
+        try:
+                result = urllib2.urlopen(req).read()
+        except urllib2.HTTPError as e:
+                print e
+                sys.exit(1)
+        except urllib2.URLError as e:
+                print e
+                sys.exit(1)
+        except:
+                print "Unexpected error :("
+                raise
+
+        if check==True:
+                data = json.loads(result)['header']
+                if int(data['code']) != 100:
+                        print "ERROR in request:\n" + str(url) + "\n"
+                        data = convert(data)
+                        pp = pprint.PrettyPrinter(indent=0)
+                        pp.pprint(data)
+                        sys.exit(1)
+
+        return result
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def api_logout():
+        URL="https://www.simyo.es/api/logout?sessionId=" + str(sessionId)
+        result = api_request(URL,"",False)
+        if VERBOSE: print result + "\n"
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def api_login():
+        global sessionId, customerId
+
+        SIMYOPASS = simyopass()
+        URL="https://www.simyo.es/api/login?"
+        data = "user=" + SIMYO_USER + "&password=" + SIMYOPASS + "&apiSig=null"
+        result = api_request(URL,data)
+        if VERBOSE: print result + "\n"
+
+        sessionId = json.loads(result)['response']['sessionId']
+        customerId = json.loads(result)['response']['customerId']
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def subscriptions():
+        global registerDate, mainProductId, billCycleType, msisdn, subscriberId, payType
+
+        URL="https://www.simyo.es/api/subscriptions/" + str(customerId) + "?sessionId=" + str(sessionId)
+        result = api_request(URL)
+        if VERBOSE: print result + "\n"
+
+        data = json.loads(result)
+        for subscription in reversed(data['response']['subcriptions']):
+                registerDate = subscription['registerDate']
+                mainProductId = subscription['mainProductId']
+                billCycleType = subscription['billCycleType']
+                msisdn = subscription['msisdn']
+                subscriberId = subscription['subscriberId']
+                payType = subscription['subscriberId']
+                if msisdn == SIMYO_NUMB:
+                        break
+
+# source: https://github.com/poliva/random-scripts/tree/master/simyo
+def consumptionByCycle(billCycleCount=1):
+        URL="https://www.simyo.es/api/consumptionByCycle/" + str(customerId) + "?sessionId=" + str(sessionId) + "&msisdn=" + str(msisdn) + "&billCycleType=" + str(billCycleType) + "&registerDate=" + str(registerDate) + "&billCycle=" + str(billCycle) + "&billCycleCount=" + str(billCycleCount) + "&payType=" + str(payType)
+        result = api_request(URL)
+        data = json.loads(result)['response']['consumptionsByCycle'][0]
+        return data
+
 def main(argv):
+    global billCycle
+    billCycle=1
+
+    api_login()
+    subscriptions()
+    data=consumptionByCycle()
+    api_logout()
+
     # Publish a message
-    mqttc.publish("hello/world", "my message")
+    mqttc.publish("simyo/"+SIMYO_NUMB+"/consumptionsByCycle", json.dumps(data, separators=(',',':')))
+
+    sys.exit(0)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
